1e19ad7a6c09ee82ede9c88221b15ec73732d8c7
Aaron Schaum <adschaum@sherwin.com>
2018-09-14 11:26:37 -0400

    add git log to output

--- webhook-target-pull-pos-source.sh
+++ webhook-target-pull-pos-source.sh
@@ -4,5 +4,6 @@
 cd /fiscal
 #git reset --hard HEAD # reset to last pull to sync up with repo, don't do this for development box
 echo 'pull pos-source into /fiscal'
-git pull origin price-interface
+git pull origin 
 echo 'pull pos-source into /fiscal done'
+git log --name-status |head 

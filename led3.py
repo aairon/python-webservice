2230aad797b033bdb45f237c397d5ca9c5557c6c
Aaron Schaum <adschaum@sherwin.com>
2019-04-20 16:37:59 -0400

    3rd row

--- led3.py
+++ led3.py
@@ -30,7 +30,7 @@ while ii > 0:
 	GPIO.output(23, GPIO.LOW)  
 	#GPIO.output(23, GPIO.HIGH)  
 	time.sleep(i)
-	ii = ii - 1.
+
 ########################
 #####
 	GPIO.output(4, GPIO.LOW)  
@@ -47,7 +47,7 @@ while ii > 0:
 	GPIO.output(23, GPIO.LOW)  
 	#GPIO.output(23, GPIO.HIGH)  
 	time.sleep(i)
-	ii = ii - 1.
+
 ########################
 #####
 	GPIO.output(4, GPIO.LOW)  
@@ -64,7 +64,61 @@ while ii > 0:
 	GPIO.output(23, GPIO.LOW)  
 	#GPIO.output(23, GPIO.HIGH)  
 	time.sleep(i)
-	ii = ii - 1.
 ########################
+
+
+##### row 2 
+	GPIO.output(4, GPIO.LOW)  
+	GPIO.output(4, GPIO.HIGH)  
+	GPIO.output(18, GPIO.LOW)  
+	#GPIO.output(18, GPIO.HIGH)  
+	GPIO.output(17, GPIO.LOW)  
+	#GPIO.output(17, GPIO.HIGH)  
+
+	GPIO.output(27, GPIO.LOW)  
+	#GPIO.output(27, GPIO.HIGH)  
+	GPIO.output(22, GPIO.LOW)  
+	GPIO.output(22, GPIO.HIGH)  
+	GPIO.output(23, GPIO.LOW)  
+	#GPIO.output(23, GPIO.HIGH)  
+	time.sleep(i)
+
+########################
+#####
+	GPIO.output(4, GPIO.LOW)  
+	#GPIO.output(4, GPIO.HIGH)  
+	GPIO.output(18, GPIO.LOW)  
+	GPIO.output(18, GPIO.HIGH)  
+	GPIO.output(17, GPIO.LOW)  
+	#GPIO.output(17, GPIO.HIGH)  
+
+	GPIO.output(27, GPIO.LOW)  
+	#GPIO.output(27, GPIO.HIGH)  
+	GPIO.output(22, GPIO.LOW)  
+	GPIO.output(22, GPIO.HIGH)  
+	GPIO.output(23, GPIO.LOW)  
+	#GPIO.output(23, GPIO.HIGH)  
+	time.sleep(i)
+
+########################
+#####
+	GPIO.output(4, GPIO.LOW)  
+	#GPIO.output(4, GPIO.HIGH)  
+	GPIO.output(18, GPIO.LOW)  
+	#GPIO.output(18, GPIO.HIGH)  
+	GPIO.output(17, GPIO.LOW)  
+	GPIO.output(17, GPIO.HIGH)  
+
+	GPIO.output(27, GPIO.LOW)  
+	#GPIO.output(27, GPIO.HIGH)  
+	GPIO.output(22, GPIO.LOW)  
+	GPIO.output(22, GPIO.HIGH)  
+	GPIO.output(23, GPIO.LOW)  
+	#GPIO.output(23, GPIO.HIGH)  
+	time.sleep(i)
+########################
+
+	ii = ii - 1.
+
 
 GPIO.cleanup()

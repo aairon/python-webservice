81b288ba733c35ada0b46d393d73612d7ce073d3
Aaronski <aaronoelle@sbcglobal.net>
2019-04-20 22:53:57 +0000

    can't get 3x3 multiplex working

--- led3.py
+++ led3.py
@@ -9,73 +9,77 @@ GPIO.setup(17, GPIO.OUT)
 
 GPIO.setup(27, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
-GPIO.setup(24, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT)
 
 i = .5
-ii = 15
+ii = 150
 while ii > 0:
 
 #####
-	#GPIO.output(4, GPIO.LOW)  
 	GPIO.output(4, GPIO.HIGH)  
-
 	GPIO.output(18, GPIO.LOW)  
-	#GPIO.output(18, GPIO.HIGH)  
-	
 	GPIO.output(17, GPIO.LOW)  
-	#GPIO.output(17, GPIO.HIGH)  
 
-	GPIO.output(27, GPIO.LOW)  
-	#GPIO.output(27, GPIO.HIGH)  
-
+	GPIO.output(27, GPIO.HIGH)  
 	GPIO.output(22, GPIO.LOW)  
-	#GPIO.output(22, GPIO.HIGH)  
-
-	GPIO.output(24, GPIO.LOW)  
-	GPIO.output(24, GPIO.HIGH)  
+	GPIO.output(20, GPIO.LOW)  
 
 	time.sleep(i)
 
 ########################
 #####
 	GPIO.output(4, GPIO.LOW)  
-	#GPIO.output(4, GPIO.HIGH)  
-
-	#GPIO.output(18, GPIO.LOW)  
 	GPIO.output(18, GPIO.HIGH)  
-
 	GPIO.output(17, GPIO.LOW)  
-	#GPIO.output(17, GPIO.HIGH)  
 
-	GPIO.output(27, GPIO.LOW)  
-	#GPIO.output(27, GPIO.HIGH)  
-
+	GPIO.output(27, GPIO.HIGH)  
 	GPIO.output(22, GPIO.LOW)  
-	#GPIO.output(22, GPIO.HIGH)  
-
-	#GPIO.output(24, GPIO.LOW)  
-	GPIO.output(24, GPIO.HIGH)  
+	GPIO.output(20, GPIO.LOW)  
 	time.sleep(i)
 
 ########################
 #####
 	GPIO.output(4, GPIO.LOW)  
-	#GPIO.output(4, GPIO.HIGH)  
-
 	GPIO.output(18, GPIO.LOW)  
-	#GPIO.output(18, GPIO.HIGH)  
+	GPIO.output(17, GPIO.HIGH)  
 
-	#GPIO.output(17, GPIO.LOW)  
+	GPIO.output(27, GPIO.HIGH)  
+	GPIO.output(22, GPIO.LOW)  
+	GPIO.output(20, GPIO.LOW)  
+	time.sleep(i)
+########################
+
+#####
+	GPIO.output(4, GPIO.HIGH)  
+	GPIO.output(18, GPIO.LOW)  
+	GPIO.output(17, GPIO.LOW)  
+
+	GPIO.output(27, GPIO.LOW)  
+	GPIO.output(22, GPIO.HIGH)  
+	GPIO.output(20, GPIO.LOW)  
+
+	time.sleep(i)
+
+########################
+#####
+	GPIO.output(4, GPIO.LOW)  
+	GPIO.output(18, GPIO.HIGH)  
+	GPIO.output(17, GPIO.LOW)  
+
+	GPIO.output(27, GPIO.LOW)  
+	GPIO.output(22, GPIO.HIGH)  
+	GPIO.output(20, GPIO.LOW)  
+	time.sleep(i)
+
+########################
+#####
+	GPIO.output(4, GPIO.LOW)  
+	GPIO.output(18, GPIO.LOW)  
 	GPIO.output(17, GPIO.HIGH)  
 
 	GPIO.output(27, GPIO.LOW)  
-	#GPIO.output(27, GPIO.HIGH)  
-
-	GPIO.output(22, GPIO.LOW)  
-	#GPIO.output(22, GPIO.HIGH)  
-
-	#GPIO.output(24, GPIO.LOW)  
-	GPIO.output(24, GPIO.HIGH)  
+	GPIO.output(22, GPIO.HIGH)  
+	GPIO.output(20, GPIO.LOW)  
 	time.sleep(i)
 ########################
 

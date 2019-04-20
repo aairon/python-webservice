8b1fbc8ce6df09e4b4f4bcfa4d57408c4e8ca316
Aaron Schaum <adschaum@sherwin.com>
2019-04-20 16:21:46 -0400

    all off

--- led3.py
+++ led3.py
@@ -16,61 +16,17 @@ while ii > 0:
 
 #####
 	GPIO.output(4, GPIO.LOW)  
+	#GPIO.output(4, GPIO.HIGH)  
 	GPIO.output(18, GPIO.LOW)  
-	GPIO.output(17, GPIO.HIGH)  
-
-	GPIO.output(27, GPIO.HIGH)  
-	GPIO.output(22, GPIO.LOW)  
-	time.sleep(i)
-#####
-#####
-	GPIO.output(4, GPIO.LOW)  
-	GPIO.output(18, GPIO.HIGH)  
+	#GPIO.output(18, GPIO.HIGH)  
 	GPIO.output(17, GPIO.LOW)  
-
-	GPIO.output(27, GPIO.HIGH)  
-	GPIO.output(22, GPIO.LOW)  
-	time.sleep(i)
-#####
-#####
-	GPIO.output(4, GPIO.HIGH)  
-	GPIO.output(18, GPIO.LOW)  
-	GPIO.output(17, GPIO.LOW)  
-
-	GPIO.output(27, GPIO.HIGH)  
-	GPIO.output(22, GPIO.LOW)  
-	time.sleep(i)
-#####
-######################################
-#####
-	GPIO.output(4, GPIO.LOW)  
-	GPIO.output(18, GPIO.LOW)  
-	GPIO.output(17, GPIO.HIGH)  
+	#GPIO.output(17, GPIO.HIGH)  
 
 	GPIO.output(27, GPIO.LOW)  
-	GPIO.output(22, GPIO.HIGH)  
+	#GPIO.output(27, GPIO.HIGH)  
+	GPIO.output(22, GPIO.LOW)  
+	#GPIO.output(22, GPIO.HIGH)  
+	GPIO.output(23, GPIO.LOW)  
+	#GPIO.output(23, GPIO.HIGH)  
 	time.sleep(i)
 #####
-#####
-	GPIO.output(4, GPIO.LOW)  
-	GPIO.output(18, GPIO.HIGH)  
-	GPIO.output(17, GPIO.LOW)  
-
-	GPIO.output(27, GPIO.LOW)  
-	GPIO.output(22, GPIO.HIGH)  
-	time.sleep(i)
-#####
-#####
-	GPIO.output(4, GPIO.HIGH)  
-	GPIO.output(18, GPIO.LOW)  
-	GPIO.output(17, GPIO.LOW)  
-
-	GPIO.output(27, GPIO.LOW)  
-	GPIO.output(22, GPIO.HIGH)  
-	time.sleep(i)
-#####
-
-	time.sleep(i)
-	ii = ii - 1
-
-GPIO.cleanup()

8f7347b0b67d33d2481a33a94116b2320ac2c0d5
Aaronski <aaronoelle@sbcglobal.net>
2019-04-20 03:33:34 +0000

    add led

---
+++ led2.py
@@ -0,0 +1,34 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(18, GPIO.OUT)  
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
+
+i = .05
+ii = 25
+while ii > 0:
+
+	GPIO.output(18, GPIO.HIGH)  
+	time.sleep(i)
+	GPIO.output(17, GPIO.HIGH)
+	time.sleep(i)
+	GPIO.output(16, GPIO.HIGH)
+	time.sleep(i)
+	GPIO.output(15, GPIO.HIGH)
+	time.sleep(i)
+
+	GPIO.output(18, GPIO.LOW)  
+	time.sleep(i)
+	GPIO.output(17, GPIO.LOW)
+	time.sleep(i)
+	GPIO.output(16, GPIO.LOW)
+	time.sleep(i)
+	GPIO.output(15, GPIO.LOW)
+	time.sleep(i)
+	ii = ii - 1
+
+GPIO.cleanup()
